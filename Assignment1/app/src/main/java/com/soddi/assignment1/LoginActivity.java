27b2d4c7aed025e82204a4067f8b96016b60d5cb
Soddi <WeRRa89@gmail.com>
2014-11-25 23:00:15 +0100

    first major git push to repo...

--- Assignment1/app/src/main/java/com/soddi/assignment1/LoginActivity.java
+++ Assignment1/app/src/main/java/com/soddi/assignment1/LoginActivity.java
@@ -1,39 +1,74 @@
 package com.soddi.assignment1;
 
-import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
-
-public class LoginActivity extends Activity {
+public class LoginActivity extends TemplateMenuActivity {
+    public final static String EXTRA_EMAIL = "com.soddi.EMAIL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_login_container) != null) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            LoginFragment loginFragment = new LoginFragment();
+            loginFragment.setArguments(getIntent().getExtras());
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            getFragmentManager().beginTransaction().add(R.id.fragment_login_container, loginFragment).commit();
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * Called when the user clicks the login button
+     */
+    public void login(View view) {
+        Intent intent = new Intent(this, ChatActivity.class);
+        EditText editText = (EditText) findViewById(R.id.emailField);
+        String email = editText.getText().toString();
+        intent.putExtra(EXTRA_EMAIL, email);
+        startActivity(intent);
+    }
+
+    public void register(View view) {
+        RegisterFragment registerFragment = new RegisterFragment();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+
+        fragmentTransaction.replace(R.id.fragment_login_container, registerFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    public void revivePassword(View view) {
+        ForgotPasswordFragment forgotPasswordFragment = new ForgotPasswordFragment();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+
+        fragmentTransaction.replace(R.id.fragment_login_container, forgotPasswordFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+    }
+
+    @Override
+    public void about() {
+        AboutFragment aboutFragment = new AboutFragment();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_login_container, aboutFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
 }
