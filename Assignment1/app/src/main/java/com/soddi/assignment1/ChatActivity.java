83454615bff4c3f3469d2dd0769871a74968ef83
Soddi <WeRRa89@gmail.com>
2014-11-27 11:51:37 +0100

    finally got menu working as intended!

--- Assignment1/app/src/main/java/com/soddi/assignment1/ChatActivity.java
+++ Assignment1/app/src/main/java/com/soddi/assignment1/ChatActivity.java
@@ -1,6 +1,5 @@
 package com.soddi.assignment1;
 
-import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,7 +9,7 @@ import android.widget.Toast;
 
 
 public class ChatActivity extends TemplateMenuActivity {
-    AboutFragment aboutFragment;
+    private boolean fragmentExists = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +19,8 @@ public class ChatActivity extends TemplateMenuActivity {
         Intent intent = getIntent();
         String email = intent.getStringExtra(LoginActivity.EXTRA_EMAIL);
 
-        if(!email.isEmpty()) {
+        if (!email.isEmpty()) {
             Toast.makeText(this, "Welcome " + email + "!", Toast.LENGTH_SHORT).show();
-        }
-        if(findViewById(R.id.fragment_chat_container) != null) {
-            AboutFragment aboutFragment = new AboutFragment();
-            aboutFragment.setArguments(getIntent().getExtras());
-            getFragmentManager().beginTransaction().add(R.id.fragment_chat_container, aboutFragment).commit();
         }
     }
 
@@ -46,7 +40,7 @@ public class ChatActivity extends TemplateMenuActivity {
         // as you specify a parent activity in AndroidManifest.xml.
 
         //noinspection SimplifiableIfStatement
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case R.id.action_about:
                 about();
                 return true;
@@ -60,23 +54,43 @@ public class ChatActivity extends TemplateMenuActivity {
 
     public void settings() {
 
-        if(findViewById(R.id.fragment_chat_container) != null) {
+        if (findViewById(R.id.fragment_chat_container) != null) {
             SettingsFragment settingsFragment = new SettingsFragment();
             settingsFragment.setArguments(getIntent().getExtras());
-            getFragmentManager().beginTransaction().add(R.id.fragment_chat_container, settingsFragment).commit();
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            if (fragmentExists) {
+                fragmentTransaction.replace(R.id.fragment_chat_container, settingsFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            } else {
+                fragmentTransaction.add(R.id.fragment_chat_container, settingsFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                fragmentExists = true;
+            }
         }
-        Toast.makeText(this, "Settings fragment created", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "Settings fragment created", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void about() {
         //Toast.makeText(this, "YES, this works!", Toast.LENGTH_SHORT).show();
 
-        if(findViewById(R.id.fragment_chat_container) != null) {
+        if (findViewById(R.id.fragment_chat_container) != null) {
             AboutFragment aboutFragment = new AboutFragment();
             aboutFragment.setArguments(getIntent().getExtras());
-            getFragmentManager().beginTransaction().add(R.id.fragment_chat_container, aboutFragment).commit();
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            if (fragmentExists) {
+                fragmentTransaction.replace(R.id.fragment_chat_container, aboutFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            } else {
+                fragmentTransaction.add(R.id.fragment_chat_container, aboutFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                fragmentExists = true;
+            }
         }
-        Toast.makeText(this, "About fragment created", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "About fragment created", Toast.LENGTH_SHORT).show();
     }
 }
--- Assignment1/app/src/main/java/com/soddi/assignment1/LoginFragment.java
+++ Assignment1/app/src/main/java/com/soddi/assignment1/LoginFragment.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link LoginFragment.OnFragmentInteractionListener} interface
+ * {@link /LoginFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link LoginFragment#newInstance} factory method to
  * create an instance of this fragment.
