c0b251de085e1a07b1a930ba334ca3f805b61d65
Soddi <WeRRa89@gmail.com>
2014-12-17 12:10:37 +0100

    more UI components, and added listeners for the buttons

--- BudgetApplication/app/src/main/java/com/soddi/budgetapplication/ExpensesFragment.java
+++ BudgetApplication/app/src/main/java/com/soddi/budgetapplication/ExpensesFragment.java
@@ -1,12 +1,14 @@
 package com.soddi.budgetapplication;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class ExpensesFragment extends Fragment {
@@ -24,7 +26,22 @@ public class ExpensesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_expenses, container, false);
+        View view = inflater.inflate(R.layout.fragment_expenses, container, false);
+
+        Button button = (Button) view.findViewById(R.id.button_Expenses);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view1) {
+                AddExpenseFragment addExpenseFragment = new AddExpenseFragment();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.layout_main, addExpenseFragment);
+                ft.addToBackStack(null);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            }
+        });
+
+        return view;
     }
 
 }
--- BudgetApplication/app/src/main/java/com/soddi/budgetapplication/IncomeFragment.java
+++ BudgetApplication/app/src/main/java/com/soddi/budgetapplication/IncomeFragment.java
@@ -1,12 +1,14 @@
 package com.soddi.budgetapplication;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class IncomeFragment extends Fragment {
 
@@ -24,7 +26,23 @@ public class IncomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_income, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_income, container, false);
+
+        Button button = (Button) view.findViewById(R.id.button_Income);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view1) {
+                AddIncomeFragment addIncomeFragment = new AddIncomeFragment();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.layout_main, addIncomeFragment);
+                ft.addToBackStack(null);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            }
+        });
+
+        return view;
     }
 
 }
