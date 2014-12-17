50b4e9bc360b5133fb03c36bd09d8de8520fc9d8
Soddi <WeRRa89@gmail.com>
2014-12-17 12:42:00 +0100

    Added listeners to all buttons

--- BudgetApplication/app/src/main/java/com/soddi/budgetapplication/AddExpenseFragment.java
+++ BudgetApplication/app/src/main/java/com/soddi/budgetapplication/AddExpenseFragment.java
@@ -1,12 +1,15 @@
 package com.soddi.budgetapplication;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class AddExpenseFragment extends Fragment {
 
@@ -23,6 +26,37 @@ public class AddExpenseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_expense, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_expense, container, false);
+
+        Button cancelExpense = (Button) view.findViewById(R.id.button_cancel_expense);
+        cancelExpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view1) {
+                Toast.makeText(getActivity(), "Canceled", Toast.LENGTH_SHORT).show();
+                ExpensesFragment expensesFragment = new ExpensesFragment();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.layout_main, expensesFragment);
+                ft.addToBackStack(null);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            }
+        });
+
+        Button addExpense = (Button) view.findViewById(R.id.button_add_expense);
+        addExpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view1) {
+                //TODO: Add income to database
+                Toast.makeText(getActivity(), "Expense added!", Toast.LENGTH_SHORT).show();
+                ExpensesFragment expensesFragment = new ExpensesFragment();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.layout_main, expensesFragment);
+                ft.addToBackStack(null);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            }
+        });
+
+        return view;
     }
 }
--- BudgetApplication/app/src/main/java/com/soddi/budgetapplication/AddIncomeFragment.java
+++ BudgetApplication/app/src/main/java/com/soddi/budgetapplication/AddIncomeFragment.java
@@ -1,12 +1,15 @@
 package com.soddi.budgetapplication;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class AddIncomeFragment extends Fragment {
 
@@ -23,6 +26,38 @@ public class AddIncomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_income, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_add_income, container, false);
+
+        Button cancelIncome = (Button) view.findViewById(R.id.button_cancel_income);
+        cancelIncome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view1) {
+                Toast.makeText(getActivity(), "Canceled", Toast.LENGTH_SHORT).show();
+                IncomeFragment incomeFragment = new IncomeFragment();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.layout_main, incomeFragment);
+                ft.addToBackStack(null);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            }
+        });
+
+        Button addIncome = (Button) view.findViewById(R.id.button_add_income);
+        addIncome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view1) {
+                //TODO: Add income to database
+                Toast.makeText(getActivity(), "Income added!", Toast.LENGTH_SHORT).show();
+                IncomeFragment incomeFragment = new IncomeFragment();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.layout_main, incomeFragment);
+                ft.addToBackStack(null);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            }
+        });
+
+        return view;
     }
 }
