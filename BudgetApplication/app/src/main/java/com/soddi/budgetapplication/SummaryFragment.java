649172204109047ab95ddf27d19cb4543af691ae
Soddi <WeRRa89@gmail.com>
2014-12-20 17:20:09 +0100

    added an external library for charts, and added graphical representation of data!

--- BudgetApplication/app/src/main/java/com/soddi/budgetapplication/SummaryFragment.java
+++ BudgetApplication/app/src/main/java/com/soddi/budgetapplication/SummaryFragment.java
@@ -1,22 +1,30 @@
 package com.soddi.budgetapplication;
 
-import android.app.Activity;
-import android.database.Cursor;
-import android.net.Uri;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.Legend;
+
+import java.util.ArrayList;
 
 public class SummaryFragment extends Fragment {
 
     private DBController dbController;
     private TextView textview_totalIncome, textview_totalExpense, textview_summary;
     private int totalIncome, totalExpense, summary = 0;
+
+    private PieChart chart;
 
     public SummaryFragment() {
         // Required empty public constructor
@@ -48,14 +56,62 @@ public class SummaryFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_summary, container, false);
 
-        textview_totalIncome = (TextView) view.findViewById(R.id.texViewIncome);
-        textview_totalExpense = (TextView) view.findViewById(R.id.texViewExpense);
-        textview_summary = (TextView) view.findViewById(R.id.texViewSummary);
+        chart = (PieChart) view.findViewById(R.id.chart);
+        String summary = "Summary: " + (dbController.getTotalIncome() + dbController.getTotalExpenses()) + " Kr";
 
-        textview_totalIncome.setText(dbController.getTotalIncome() + " kr");
-        textview_totalExpense.setText(dbController.getTotalExpenses() + " kr");
-        textview_summary.setText((dbController.getTotalIncome() + dbController.getTotalExpenses()) + " kr");
+
+        //PieChart External library
+        // change the color of the center-hole
+        chart.setHoleColor(Color.rgb(235, 235, 235));
+        chart.setHoleRadius(60f);
+        chart.setDescription("");
+        chart.setDrawYValues(true);
+        chart.setDrawXValues(true);
+        chart.setDrawCenterText(true);
+        chart.setDrawHoleEnabled(true);
+        chart.setRotation(0f);
+        chart.setRotationEnabled(true);
+        chart.setCenterText("Summary");
+        setData(dbController.getTotalIncome(), dbController.getTotalExpenses());
+        chart.animateXY(2000, 2000);
+
+        Legend one = chart.getLegend();
+        one.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
+        one.setXEntrySpace(7f);
+        one.setYEntrySpace(5f);
+
+        chart.setCenterText(summary);
 
         return view;
     }
+
+    private void setData(int income, int expense) {
+
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+        // IMPORTANT: In a PieChart, no values (Entry) should have the same
+        // xIndex (even if from different DataSets), since no values can be
+        // drawn above each other.
+        yVals.add(new Entry((float) income, 0));
+        yVals.add(new Entry((float) expense, 1));
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        xVals.add("Incomes");
+        xVals.add("Expenses");
+
+        PieDataSet set1 = new PieDataSet(yVals, "Transactions");
+        set1.setSliceSpace(3f);
+
+        int darkRed = getResources().getColor(android.R.color.holo_red_dark);
+        int darkGreen = getResources().getColor(android.R.color.holo_green_dark);
+        set1.setColors(new int[] {darkGreen, darkRed});
+
+        PieData data = new PieData(xVals, set1);
+        chart.setData(data);
+
+        // undo all highlights
+        chart.highlightValues(null);
+
+        chart.invalidate();
+    }
+
 }
