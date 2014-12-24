07927cca3b01952ea63476ed948fca0b8f759bb9
Soddi <WeRRa89@gmail.com>
2014-12-24 23:13:01 +0100

    huge update, though it crasches because of an invalid listview

---
+++ MediaSensorApp/app/src/main/java/com/soddi/mediasensorapp/CustomAdapterSong.java
@@ -0,0 +1,38 @@
+package com.soddi.mediasensorapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by soddi on 2014-12-24.
+ */
+public class CustomAdapterSong extends ArrayAdapter<Integer> {
+
+    private final Context context;
+    private ArrayList<Integer> songs;
+
+    public CustomAdapterSong(Context context, ArrayList<Integer> songs) {
+        super(context, R.layout.custom_listview_layout, songs);
+        this.context = context;
+        this.songs = songs;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.custom_listview_layout, parent, false);
+        TextView textView = (TextView) view.findViewById(R.id.list_TextView);
+        ImageView imageView = (ImageView) view.findViewById(R.id.list_ImageView);
+        textView.setText(songs.get(position).toString());
+        imageView.setImageResource(R.drawable.ic_action_headphones);
+        return view;
+    }
+}
--- MediaSensorApp/app/src/main/java/com/soddi/mediasensorapp/MainActivity.java
+++ MediaSensorApp/app/src/main/java/com/soddi/mediasensorapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.soddi.mediasensorapp;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -12,8 +13,10 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 
-public class MainActivity extends Activity implements SensorEventListener {
+
+public class MainActivity extends ListActivity implements SensorEventListener {
 
     Sensor sensor;
     SensorManager sensorManager;
@@ -21,11 +24,17 @@ public class MainActivity extends Activity implements SensorEventListener {
     private Toast toast;
     private boolean knockActivated = false;
 
-    private final static double KNOCK_THRESHOLD = 1.5;
-    private long knockGestureTimeInSeconds = 1500;
+    private final static double KNOCK_THRESHOLD = 0.2;
+    private long knockingTime = 1250;
+    private final static int SAMPLING_TIME = 20;
     private float last_z = 0;
+    private long lastUpdate = 0;
     private long timeFirstKnock = 0;
     int amountOfKnocks = 0;
+    float avgValue = 0;
+    private ArrayList<Float> arraylistValues = new ArrayList<Float>();
+    private ArrayList<Integer> songList = new ArrayList<Integer>();
+    int songCounter = 0;
 
     MediaPlayer mPlayer;
 
@@ -38,9 +47,22 @@ public class MainActivity extends Activity implements SensorEventListener {
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
         displayReading = (TextView) findViewById(R.id.textView_SensorReading);
+        songList.add(R.raw.game_of_thrones);
+        songList.add(R.raw.the_big_bang_theory);
+        songList.add(R.raw.the_simpsons);
 
+        CustomAdapterSong adapterSong = new CustomAdapterSong(this, songList);
+        setListAdapter(adapterSong);
     }
 
+    private int chooseSong() {
+        if (songCounter == 3) {
+            songCounter = 0;
+        }
+        songCounter++;
+        int song = songList.get(songCounter);
+        return song;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -67,55 +89,71 @@ public class MainActivity extends Activity implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
         float zValue = event.values[2];
-        float knockValue = Math.abs(zValue - last_z);
-        long currentTime = System.currentTimeMillis();
-        displayReading.setText("Z: " + knockValue);
+        arraylistValues.add(zValue);
 
-        if (knockValue > KNOCK_THRESHOLD) {
-            timeFirstKnock = System.currentTimeMillis();
-            knockActivated = true;
-            amountOfKnocks++;
+        long currentTime = System.currentTimeMillis();
+
+        if ((currentTime - lastUpdate) > SAMPLING_TIME) {
+            lastUpdate = currentTime;
+            for (Float value : arraylistValues) {
+                avgValue += value;
+            }
+            if (amountOfKnocks > 4) {
+                knockActivated = false;
+                amountOfKnocks = 0;
+                timeFirstKnock = 0;
+            }
+            avgValue /= arraylistValues.size();
+            avgValue = Math.abs(avgValue - last_z);
+            displayReading.setText("Z: " + avgValue + "\nNumberOfKnocks: " + amountOfKnocks);
+            if (avgValue > KNOCK_THRESHOLD) {
+                timeFirstKnock = System.currentTimeMillis();
+                knockActivated = true;
+                amountOfKnocks++;
+            }
+            avgValue = 0;
+            last_z = zValue;
+
         }
-        last_z = zValue;
-        if (currentTime - timeFirstKnock > knockGestureTimeInSeconds && knockActivated) {
+        if (currentTime - timeFirstKnock > knockingTime && knockActivated) {
             switch (amountOfKnocks) {
                 case 1:
-                    Toast.makeText(this, "Stopping music (" + amountOfKnocks + ")", Toast.LENGTH_SHORT).show();
-                    if(mPlayer!=null && mPlayer.isPlaying()){//If music is playing already
-                        mPlayer.stop();//Stop playing the music
+                    if (mPlayer == null) {
+                        Toast.makeText(this, "Starting music (" + amountOfKnocks + ")", Toast.LENGTH_SHORT).show();
+                        mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.game_of_thrones);
+                        mPlayer.start();//Start playing the music
+                    } else {
+                        Toast.makeText(this, "Pausing music (" + amountOfKnocks + ")", Toast.LENGTH_SHORT).show();
+                        mPlayer.pause();
                     }
                     break;
                 case 2:
-                    Toast.makeText(this, "Starting music (" + amountOfKnocks + ")", Toast.LENGTH_SHORT).show();
-                    mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.game_of_thrones);
-                    mPlayer.start();//Start playing the music
+                    Toast.makeText(this, "Next song (" + amountOfKnocks + ")", Toast.LENGTH_SHORT).show();
+                    if (mPlayer != null) {//If music is playing already
+                        mPlayer.stop();//Stop playing the music
+                        mPlayer = MediaPlayer.create(getApplicationContext(), chooseSong());
+                        mPlayer.start();//Start playing the music
+                    }
                     break;
                 case 3:
-                    Toast.makeText(this, "Pausing music (" + amountOfKnocks + ")", Toast.LENGTH_SHORT).show();
-                    if(mPlayer!=null && mPlayer.isPlaying()){//If music is playing already
-                        mPlayer.pause();//Stop playing the music
+                    Toast.makeText(this, "previous song (" + amountOfKnocks + ")", Toast.LENGTH_SHORT).show();
+                    if (mPlayer != null) {//If music is playing already
+                        mPlayer.stop();//Stop playing the music
+                        mPlayer = MediaPlayer.create(getApplicationContext(), chooseSong());
+                        mPlayer.start();//Start playing the music
                     }
                     break;
                 case 4:
-                    Toast.makeText(this, "Next song (" + amountOfKnocks + ")", Toast.LENGTH_SHORT).show();
-                    if(mPlayer!=null){//If music is playing already
+                    Toast.makeText(this, "Stopping music (" + amountOfKnocks + ")", Toast.LENGTH_SHORT).show();
+                    if (mPlayer != null && mPlayer.isPlaying()) {//If music is playing already
                         mPlayer.stop();//Stop playing the music
-                        mPlayer = MediaPlayer.create(getApplicationContext(),R.raw.the_simpsons);
-                        mPlayer.start();//Start playing the music
-                    }
-                    break;
-                case 5:
-                    Toast.makeText(this, "previous song (" + amountOfKnocks + ")", Toast.LENGTH_SHORT).show();
-                    if(mPlayer!=null){//If music is playing already
-                        mPlayer.stop();//Stop playing the music
-                        mPlayer = MediaPlayer.create(getApplicationContext(),R.raw.the_big_bang_theory);
-                        mPlayer.start();//Start playing the music
                     }
                     break;
                 default:
-                    toast.makeText(this, "Amount of knocks: " + amountOfKnocks, Toast.LENGTH_SHORT).show();
+                    toast.makeText(this, "Too many knocks: " + amountOfKnocks, Toast.LENGTH_SHORT).show();
                     break;
             }
+            avgValue = 0;
             knockActivated = false;
             amountOfKnocks = 0;
             timeFirstKnock = 0;
